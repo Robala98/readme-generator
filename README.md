d242574a0e4d1320d207312ac54a77c9db68f936
Robala <robala998@gmail.com>
2022-05-09 18:58:13 -0500

    finished

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# readme-generator
+# ReadME generator
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Table of Contents
+ [Description](#description)
+ [Installation](#installation)
+ [Usage](#usage)
+ [Contribution](#contribution)
+ [license ](#license)
+ [Test](#test)
+ [Questions](#questions)
+
+ ## Description
+
+ This project creates a ReadME file to describe a project.
+
+ ## Instalation
+
+ undefined 
+
+ ## Usage
+
+ Answer the questions to fill in the file.
+
+ ## License 
+
+ MIT
+
+ license wedsite:(https://opensource.org/licenses/MIT)
+
+ ## Contributions
+
+ no
+
+ ## Test
+
+ no
+
+ ## Questions
+
+ Here is a [link](https://www.github.com/Robala98) to my GitHub profile,
+for questions, email robala998@gmail.com 
+
+# Heres a working video of the application :p
+
+
--- index.js
+++ index.js
@@ -1,13 +1,74 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown")
 
-// TODO: Create an array of questions for user input
-const questions = [];
+function init(){
+inquirer.prompt([
+    
+    {
+      type: 'input',
+      message: 'What is the name of your project?',
+      name: 'title',
+    },
+    
+    {
+      type: 'input',
+      message: 'Give a brief description of your project:',
+      name: 'description',
+    },
+    
+    {
+      type: 'input',
+      message: 'Give directions on how to use your project:',
+      name: 'usage',
+    },
+    
+    {
+        type: 'input',
+        message: 'Instructions for installation?',
+        name: 'installation',
+      },
+      
+    {
+        type:'list',
+        message: 'What license if any will you be using?',
+        name:'license',
+        choices:[
+            'Apache',
+            'MIT',
+            'Mozilla',
+            'NONE'
+        ]
+      },
+      
+      {
+        type: 'input',
+        message: 'Any other contributers?',
+        name: 'contributions',
+      },
+      {
+        type: 'input',
+        message: 'Is there any testing implemented?',
+        name: 'test',
+      },
+      {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: 'Enter your github username:',
+        name: 'github',
+      },
+      
+      
+  ])
+  .then((response) => {
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
+    fs.writeFileSync('README.md', generateMarkdown(response))
+    
+  })
+}
 // Function call to initialize app
 init();
